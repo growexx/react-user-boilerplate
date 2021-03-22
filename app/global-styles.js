be97cf8b0bf687c54d85f73ba9fb49be88d2152d
pratibha-growexx <pratibha.hotwani@growexx.com>
2021-03-22 23:40:46 +0530

    refactor: 💡 changed variable names and font export changes

--- app/global-styles.js
+++ app/global-styles.js
@@ -1,7 +1,18 @@
 import { createGlobalStyle } from 'styled-components';
 import 'antd/dist/antd.css';
+import {
+  EPILOGUE_STAMP_WOFF2,
+  EPILOGUE_STAMP_WOFF,
+} from './styles/fonts/Epilogue';
+
+import injectFontFaces from './styles/injectFontFaces';
+const getFormattedURL = (name, format) => `url(${name}) format(${format})`;
+
 const GlobalStyle = createGlobalStyle`
-  html,
+  ${injectFontFaces('Epilogue', [
+    getFormattedURL(EPILOGUE_STAMP_WOFF2, 'woff2'),
+    getFormattedURL(EPILOGUE_STAMP_WOFF, 'woff'),
+  ])}
   body {
     height: 100%;
     width: 100%;
