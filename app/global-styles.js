c0d9eca823647eb3b70686c764a1f985f0c41354
pratibha-growexx <pratibha.hotwani@growexx.com>
2021-03-22 18:10:56 +0530

    feat: 🎸 added dynamic function to add font-faces

--- app/global-styles.js
+++ app/global-styles.js
@@ -1,13 +1,23 @@
 import { createGlobalStyle } from 'styled-components';
 import 'antd/dist/antd.css';
 import theme, { fontFamily, lineHeights } from './styles/index';
+import InjectFontFaces from './styles/injectFontFaces';
+import EPILOGUE_STAMP_WOFF2 from './styles/fonts/Epilogue/Epilogue_Stamp.woff2';
+import EPILOGUE_STAMP_WOFF from './styles/fonts/Epilogue/Epilogue_Stamp.woff';
+
+const getFontURLFormat = (name, format) => `url(${name}) format(${format})`;
+
 const GlobalStyle = createGlobalStyle`
+  ${InjectFontFaces('Epilogue', [
+    getFontURLFormat(EPILOGUE_STAMP_WOFF2, 'woff2'),
+    getFontURLFormat(EPILOGUE_STAMP_WOFF, 'woff'),
+  ])}
   html,
   body {
     height: 100%;
     width: 100%;
     line-height: ${lineHeights.standard};
-    font-family: ${fontFamily.fontRubik}
+    font-family: ${fontFamily[0].fontRubik};
   }
 
   #app {
