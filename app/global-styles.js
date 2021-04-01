031f166f6a0862e37473846f11c5427986f5e191
pratibha-growexx <pratibha.hotwani@growexx.com>
2021-04-01 15:36:53 +0530

    style: 💄 ant variables added for override

--- app/global-styles.js
+++ app/global-styles.js
@@ -6,7 +6,26 @@ import {
 
 import injectFontFaces from './styles/injectFontFaces';
 const getFormattedURL = (name, format) => `url(${name}) format(${format})`;
-const utilityClasses = `
+const GlobalStyle = createGlobalStyle`
+  ${injectFontFaces('Roboto', [
+    getFormattedURL(ROBOTO_BLACK_TTF, 'ttf'),
+    getFormattedURL(ROBOTO_BOLD_ITALIC_TTF, 'ttf'),
+  ])}
+  body {
+    height: 100%;
+    width: 100%;
+  }
+  body {
+    font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  }
+  body.fontLoaded {
+    font-family: 'Open Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  }
+  #app {
+    background-color: #fafafa;
+    min-height: 100%;
+    min-width: 100%;
+  }
 /**
 * display utility class
 */
@@ -533,28 +552,7 @@ const utilityClasses = `
 .u-text-break {
  word-break: break-word !important;
  overflow-wrap: break-word !important;
-}`;
-const GlobalStyle = createGlobalStyle`
-  ${injectFontFaces('Roboto', [
-    getFormattedURL(ROBOTO_BLACK_TTF, 'ttf'),
-    getFormattedURL(ROBOTO_BOLD_ITALIC_TTF, 'ttf'),
-  ])}
-  body {
-    height: 100%;
-    width: 100%;
-  }
-  body {
-    font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
-  }
-  body.fontLoaded {
-    font-family: 'Open Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;
-  }
-  #app {
-    background-color: #fafafa;
-    min-height: 100%;
-    min-width: 100%;
-  }
-  ${utilityClasses};
+}
 `;
 
 export default GlobalStyle;
