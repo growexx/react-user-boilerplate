2a7273d5d1bb1362c700f6298b678b2c37c3ee30
pratibha-growexxer <pratibha.hotwani@growexx.com>
2021-04-14 16:45:36 +0530

    feat: 🎸 react-final-form demo added

--- app/examples/ReactFinalForm/index.js
+++ app/examples/ReactFinalForm/index.js
@@ -4,44 +4,143 @@
  *
  */
 
-import React, { memo } from 'react';
-// import { Form, Field } from 'react-final-form'
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import React from 'react';
+import { Form, Field } from 'react-final-form';
+import { Form as AntdForm, Button, Radio, Select } from 'antd';
 import { Helmet } from 'react-helmet';
-import { FormattedMessage } from 'react-intl';
-import { compose } from 'redux';
+import * as formValidations from 'utils/formValidations';
+import {
+  AInput,
+  ARangePicker,
+  ARadioGroup,
+  ACheckbox,
+  ASelect,
+  ATextarea,
+} from 'utils/Fields';
+const { Option } = Select;
+const FormItem = AntdForm.Item;
+const onSubmit = values => {
+  // eslint-disable-next-line no-console
+  console.log(JSON.stringify(values, 0, 2));
+};
 
-import messages from './messages';
-
-export function ReactFinalForm() {
-  return (
+const ReactFinalForm = () => (
+  <div>
     <div>
       <Helmet>
         <title>ReactFinalForm</title>
         <meta name="description" content="Description of ReactFinalForm" />
       </Helmet>
-      <FormattedMessage {...messages.header} />
     </div>
-  );
-}
+    <Form
+      onSubmit={onSubmit}
+      render={({
+        handleSubmit,
+        submitting,
+        pristine,
+        form,
+        invalid,
+        values,
+      }) => (
+        <>
+          <AntdForm onFinish={handleSubmit} onSubmit={handleSubmit}>
+            <Field
+              label="First Name"
+              name="firstName"
+              component={AInput}
+              placeholder="First Name"
+              hasFeedback
+              validate={formValidations.required}
+            />
 
-ReactFinalForm.propTypes = {
-  dispatch: PropTypes.func.isRequired,
-};
+            <Field
+              label="Last Name"
+              name="lastName"
+              component={AInput}
+              placeholder="Last Name"
+              validate={formValidations.required}
+            />
 
-function mapDispatchToProps(dispatch) {
-  return {
-    dispatch,
-  };
-}
+            <Field
+              label="Email"
+              name="email"
+              component={AInput}
+              type="email"
+              placeholder="Email"
+              validate={(formValidations.required, formValidations.validEmail)}
+            />
 
-const withConnect = connect(
-  null,
-  mapDispatchToProps,
+            <Field
+              label="Sex"
+              name="sex"
+              type="radio"
+              value={values.sex}
+              component={ARadioGroup}
+            >
+              <Radio value="male">Male</Radio>
+              <Radio value="female">Female</Radio>
+            </Field>
+
+            <Field
+              label="Favorite Color"
+              name="favoriteColor"
+              component={ASelect}
+            >
+              <Option value="ff0000">Red</Option>
+              <Option value="00ff00">Green</Option>
+              <Option value="0000ff">Blue</Option>
+            </Field>
+
+            <Field
+              label="Employed"
+              name="employed"
+              id="employed"
+              component={ACheckbox}
+              type="checkbox"
+            />
+
+            <Field
+              label="Filter dates"
+              name="rangepicker"
+              component={ARangePicker}
+              placeholder={['From', 'To']}
+              hasFeedback
+              onFocus={e => e.preventDefault()}
+              onBlur={e => e.preventDefault()}
+            />
+
+            <Field
+              data-testid="Notes"
+              label="Notes"
+              name="notes"
+              component={ATextarea}
+            />
+
+            <FormItem>
+              <center>
+                <Button
+                  type="primary"
+                  disabled={pristine || submitting}
+                  htmlType="submit"
+                  style={{ marginRight: '10px' }}
+                  onClick={handleSubmit}
+                >
+                  Submit
+                </Button>
+
+                <Button
+                  disabled={pristine || submitting || invalid}
+                  onClick={form.reset}
+                >
+                  Clear Values
+                </Button>
+              </center>
+            </FormItem>
+          </AntdForm>
+        </>
+      )}
+    />
+  </div>
 );
 
-export default compose(
-  withConnect,
-  memo,
-)(ReactFinalForm);
+export default ReactFinalForm;
--- app/examples/ReactFinalForm/tests/index.test.js
+++ app/examples/ReactFinalForm/tests/index.test.js
@@ -1,48 +1,98 @@
 /**
  *
  * Tests for ReactFinalForm
- *
- * @see https://github.com/react-boilerplate/react-boilerplate/tree/master/docs/testing
- *
  */
 
 import React from 'react';
-import { render } from 'react-testing-library';
+import { fireEvent, render, queryByAttribute } from 'react-testing-library';
 import { IntlProvider } from 'react-intl';
-// import 'jest-dom/extend-expect'; // add some helpful assertions
-
-import { ReactFinalForm } from '../index';
-import { DEFAULT_LOCALE } from '../../../i18n';
-
+import { browserHistory } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import history from 'utils/history';
+import { ConnectedRouter } from 'connected-react-router';
+import ReactFinalForm from '../index';
+import configureStore from '../../../configureStore';
+let store;
+const componentWrapper = () =>
+  render(
+    <Provider store={store}>
+      <IntlProvider locale="en">
+        <ConnectedRouter history={history}>
+          <ReactFinalForm />
+        </ConnectedRouter>
+      </IntlProvider>
+    </Provider>,
+  );
 describe('<ReactFinalForm />', () => {
-  it('Expect to not log errors in console', () => {
-    const spy = jest.spyOn(global.console, 'error');
-    const dispatch = jest.fn();
-    render(
-      <IntlProvider locale={DEFAULT_LOCALE}>
-        <ReactFinalForm dispatch={dispatch} />
-      </IntlProvider>,
-    );
-    expect(spy).not.toHaveBeenCalled();
+  beforeEach(() => {
+    store = configureStore({}, browserHistory);
   });
-
-  it('Expect to have additional unit tests specified', () => {
-    expect(true).toEqual(false);
-  });
-
-  /**
-   * Unskip this test to use it
-   *
-   * @see {@link https://jestjs.io/docs/en/api#testskipname-fn}
-   */
-  it.skip('Should render and match the snapshot', () => {
+  it('Should render and match the snapshot', () => {
     const {
       container: { firstChild },
-    } = render(
-      <IntlProvider locale={DEFAULT_LOCALE}>
-        <ReactFinalForm />
-      </IntlProvider>,
-    );
+    } = componentWrapper();
     expect(firstChild).toMatchSnapshot();
+  });
+  it('Should change form fields', () => {
+    const eventObject = {
+      preventDefault: jest.fn(),
+      target: {
+        value: 'test',
+        name: 'test',
+        checked: true,
+      },
+    };
+    const {
+      getByPlaceholderText,
+      getByTestId,
+      getByRole,
+      container,
+      getByText,
+    } = componentWrapper();
+    const getById = queryByAttribute.bind(null, 'id');
+    fireEvent.change(getByPlaceholderText('First Name'), eventObject);
+    fireEvent.change(getByPlaceholderText('Last Name'), eventObject);
+    fireEvent.change(getByPlaceholderText('Email'), {
+      target: {
+        value: 'it@growexx.com',
+        name: 'email',
+      },
+    });
+    fireEvent.click(getById(container, 'employed'), eventObject);
+    fireEvent.mouseDown(getByRole('combobox'));
+    fireEvent.change(getByRole('combobox'), {
+      target: {
+        value: 'ff0000',
+      },
+    });
+    // .ant-select-item-option-content
+    fireEvent.click(
+      document.querySelectorAll('.ant-select-item-option-content')[0],
+    );
+    fireEvent.change(getByTestId('Notes'), eventObject);
+    fireEvent.focus(getByPlaceholderText('From'), eventObject);
+    fireEvent.blur(getByPlaceholderText('From'), eventObject);
+    // onChange from
+    fireEvent.mouseDown(getByPlaceholderText('From'));
+    fireEvent.change(getByPlaceholderText('From'), {
+      target: {
+        value: '2020-10-12',
+      },
+    });
+    fireEvent.click(document.querySelectorAll('.ant-picker-cell-selected')[0]);
+    // onChange To
+    const to = getByPlaceholderText('To');
+    fireEvent.mouseDown(to);
+    fireEvent.change(to, {
+      target: {
+        value: '2021-10-28',
+      },
+    });
+    fireEvent.click(document.querySelectorAll('.ant-picker-cell-selected')[0]);
+    expect(getByPlaceholderText('First Name')).toBeTruthy();
+    // submit click
+    const button = getByText('Submit');
+    fireEvent.click(button);
+    // expect(props.handleSubmit).toBeCalled();
   });
 });
