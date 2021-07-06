46694114a192c7efac6b7580e55dc7f1b25ebba8
pratibha-growexxer <pratibha.hotwani@growexx.com>
2021-07-06 10:01:51 +0530

    test: 💍 chat list test cases wip

--- app/examples/RealTimeChat/stub/index.js
+++ app/examples/RealTimeChat/stub/index.js
@@ -1,15 +1,3 @@
-export const getSuccessMockSearchResults = () => {
-  const data = [];
-  // eslint-disable-next-line no-plusplus
-  for (let i = 0; i < 10; i++) {
-    data.push({
-      data: () => ({ email: `johndoe_${i}@gmail.com` }),
-    });
-  }
-
-  return Promise.resolve(data);
-};
-export const getFailureMockSearchResults = () => Promise.reject(new Error());
 const personOne = {
   id: '1',
 };
@@ -22,6 +10,40 @@ export const chatWindowStub = {
   createdBy: '',
   joined: [personOne, personTwo],
 };
+export const getSuccessMockSearchResults = () => {
+  const data = [];
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < 10; i++) {
+    data.push({
+      data: () => ({ email: `johndoe_${i}@gmail.com` }),
+    });
+  }
+
+  return Promise.resolve(data);
+};
+export const getFailureMockSearchResults = () => Promise.reject(new Error());
+
+export const getSuccessMockChatList = () => {
+  const docs = [];
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < 10; i++) {
+    docs.push({
+      data: () => chatWindowStub,
+    });
+  }
+
+  return { docs };
+};
+export const getFailureMockChatList = () => Promise.reject(new Error());
+
+export const getSuccessDataFromReference = () => {
+  const data = {
+    data: () => ({ email: `johndoe_1@gmail.com`, userName: `johndoe_1` }),
+  };
+
+  return Promise.resolve(data);
+};
+export const getFailureDataFromReference = () => Promise.reject(new Error());
 
 export const TEST_IDS = {
   OPEN_CHAT_WINDOW: 'OPEN_CHAT_WINDOW',
--- app/examples/RealTimeChat/tests/index.test.js
+++ app/examples/RealTimeChat/tests/index.test.js
@@ -16,22 +16,30 @@ import { browserHistory } from 'react-router-dom';
 import { ConnectedRouter } from 'connected-react-router';
 import configureStore from 'configureStore';
 import RealTimeChat from 'examples/RealTimeChat/index';
-import { getFireStoreCollectionReference } from 'utils/firebase';
+import {
+  getFireStoreCollectionReference,
+  getDataFromReference,
+} from 'utils/firebase';
 import {
   chatWindowStub,
   getSuccessMockSearchResults,
   getFailureMockSearchResults,
+  getSuccessMockChatList,
+  getSuccessDataFromReference,
+  getFailureDataFromReference,
 } from 'examples/RealTimeChat/stub';
 jest.mock('utils/firebase');
 jest.mock('utils/Helper', () => ({
   getUserData: jest.fn().mockReturnValue({
-    email: 'johnDoe@growexx.com',
+    email: 'johndoe_0@gmail.com',
   }),
 }));
-const mockFirebaseHelperFunctions = responseType => {
+const mockFirebaseHelperFunctions = async responseType => {
   getFireStoreCollectionReference.mockImplementation(() => ({
     where: jest.fn().mockImplementation(() => ({
-      onSnapshot: jest.fn(),
+      onSnapshot: jest.fn(async snapshotCallbackFunction =>
+        snapshotCallbackFunction(getSuccessMockChatList()),
+      ),
       get: jest
         .fn()
         .mockImplementationOnce(() =>
@@ -41,6 +49,11 @@ const mockFirebaseHelperFunctions = responseType => {
         ),
     })),
   }));
+  getDataFromReference.mockImplementation(() =>
+    responseType === 'success'
+      ? getSuccessDataFromReference()
+      : getFailureDataFromReference(),
+  );
 };
 let store;
 const props = {
@@ -64,27 +77,31 @@ describe('<RealTimeChat />', () => {
   afterEach(() => {
     getFireStoreCollectionReference.mockReset();
   });
-  it('Should select the value from dropdown', async () => {
-    mockFirebaseHelperFunctions('success');
-    const { getByRole, debug, getByText } = componentWrapper();
-    fireEvent.mouseDown(getByRole('combobox'));
-    fireEvent.change(getByRole('combobox'), {
-      target: {
-        value: 'johndoe_9@gmail.com',
-      },
-    });
-    debug();
-    await waitForElement(() => getByRole('option'));
-    fireEvent.click(
-      document.querySelectorAll('.ant-select-item-option-content')[0],
-    );
-    expect(getByText('johndoe_9@gmail.com')).toBeInTheDocument();
-  });
   it('Should render and match the snapshot', () => {
     mockFirebaseHelperFunctions('error');
     const {
       container: { firstChild },
     } = componentWrapper();
     expect(firstChild).toMatchSnapshot();
+  });
+  it('Should select the value from dropdown', async () => {
+    mockFirebaseHelperFunctions('success');
+    const { getByRole, getByText } = componentWrapper();
+    fireEvent.mouseDown(getByRole('combobox'));
+    fireEvent.change(getByRole('combobox'), {
+      target: {
+        value: 'johndoe_9@gmail.com',
+      },
+    });
+    await waitForElement(() => getByRole('option'));
+    fireEvent.click(
+      document.querySelectorAll('.ant-select-item-option-content')[0],
+    );
+    expect(getByText('johndoe_9@gmail.com')).toBeInTheDocument();
+  });
+  it('Should fetch the chat list and set the state values', async () => {
+    mockFirebaseHelperFunctions('success');
+    const { getByText } = componentWrapper();
+    await waitForElement(() => getByText('johndoe_1'));
   });
 });
