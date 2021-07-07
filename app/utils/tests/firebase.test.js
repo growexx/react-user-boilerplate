7135ce375abb99ce9d47aa382485f02ad76eec6c
pratibha-growexxer <pratibha.hotwani@growexx.com>
2021-07-07 11:32:44 +0530

    test: 💍 added test cases for firebase utils

--- app/utils/tests/firebase.test.js
+++ app/utils/tests/firebase.test.js
@@ -1,10 +1,50 @@
+import {
+  getFireStoreCollectionReference,
+  addFirestoreDocumentData,
+  getFireStoreDocumentReference,
+  getDataFromReference,
+  getFireStoreDocumentData,
+  setFirestoreDocumentData,
+} from 'utils/firebase';
+const stubData = {
+  collectionName: 'testCollection',
+  documentName: 'testDocument',
+  get: jest.fn(),
+  payload: {},
+  options: {},
+  documentData: {},
+};
 describe('Firebase utils', () => {
-  test('firebase signInWithGoogle', () => {
-    // signInWithGoogle();
-    // expect(signInWithGoogle).toBeTruthy();
+  test('firebase getFireStoreCollectionReference', () => {
+    getFireStoreCollectionReference(stubData.collectionName);
+    expect(getFireStoreCollectionReference).toBeTruthy();
   });
-  test('firebase signInWithFacebook', () => {
-    // signInWithFacebook();
-    // expect(signInWithFacebook).toBeTruthy();
+  test('firebase addFirestoreDocumentData', () => {
+    addFirestoreDocumentData(stubData.collectionName, stubData.documentData);
+    expect(addFirestoreDocumentData).toBeTruthy();
+  });
+  test('firebase getFireStoreDocumentReference', () => {
+    getFireStoreDocumentReference(
+      stubData.collectionName,
+      stubData.documentName,
+    );
+    expect(getFireStoreDocumentReference).toBeTruthy();
+  });
+  test('firebase getDataFromReference', () => {
+    getDataFromReference(stubData);
+    expect(getDataFromReference).toBeTruthy();
+  });
+  test('firebase getFireStoreDocumentData', () => {
+    getFireStoreDocumentData(stubData.collectionName, stubData.documentName);
+    expect(getFireStoreDocumentData).toBeTruthy();
+  });
+  test('firebase setFirestoreDocumentData', () => {
+    setFirestoreDocumentData(
+      stubData.collectionName,
+      stubData.documentName,
+      stubData.payload,
+      stubData.options,
+    );
+    expect(setFirestoreDocumentData).toBeTruthy();
   });
 });
