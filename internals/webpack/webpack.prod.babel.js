4d35fc1fc37e71e269da79f42f92da36aa5775c6
vipin-growexxer <96475454+vipin-growexxer@users.noreply.github.com>
2022-03-08 09:08:51 +0530

    Update webpack.prod.babel.js
    Index.html file should also not be cached so post deployment user will see updated content/js files

--- internals/webpack/webpack.prod.babel.js
+++ internals/webpack/webpack.prod.babel.js
@@ -94,7 +94,7 @@ module.exports = require('./webpack.base.babel')({
 
       // No need to cache .htaccess. See http://mxs.is/googmp,
       // this is applied before any match in `caches` section
-      excludes: ['.htaccess'],
+      excludes: ['.htaccess', 'index.html'],
 
       caches: {
         main: [':rest:'],
